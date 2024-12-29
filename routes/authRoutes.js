572ad1882c739e7b5289b32840d5b69692afeb99
Anish Vaidya <anish17vaidya@gmail.com>
2024-12-29 12:49:31 +0530

    Security Packages added

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,10 +2,18 @@ import express from 'express'
 import { register, login, logout, updateUser } from '../controller/authController.js'
 import { authenticateUser } from '../middleware/authMiddleware.js'
 
+import rateLimiter from 'express-rate-limit';
+
+const apiLimiter = rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 10,
+    message: { msg: 'IP rate limit exceeded, retry in 15 minutes' }
+});
+
 const router = express.Router()
 
-router.route('/register').post(register)
-router.route('/login').post(login)
+router.route('/register').post(apiLimiter, register)
+router.route('/login').post(apiLimiter, login)
 router.get('/logout', logout);
 router.route('/updateUser').patch(authenticateUser, updateUser)
 
